c3d88d28a5f3db62472a8d09a7b36e4a54564af7
brunoanken <brunoanken@gmail.com>
2019-04-05 00:25:45 -0300

    agora os plots levam em consideração a dimensão das matrizes a serem realizadas, desta forma todo o processo está automatizado

--- script/test.m
+++ script/test.m
@@ -9,12 +9,16 @@
 % end
 
 % os intervalos de minutos
-for M = 1:5
-    % S = quantidade de semanas para se fazer os cálculos
-    for S = 2:4
-    % Os dias a serem lidos
-        for D = 1:7
-            disp(strcat("../dados_rede/data/entropy/", num2str(S), "/", num2str(D)))
-        end
-    end
-end
+% for M = 1:5
+%     % S = quantidade de semanas para se fazer os cálculos
+%     for S = 2:4
+%     % Os dias a serem lidos
+%         for D = 1:7
+%             disp(strcat("../dados_rede/data/entropy/", num2str(S), "/", num2str(D)))
+%         end
+%     end
+% end1
+
+M1 = csvread("../dados_rede/data/entropy/1/1.csv", 1, 1);
+disp(size(M1, 1))
+disp(size(M1, 2))
