51d1050f9e70ac754c84ac39b444d1f6d4176e77
brunoanken <brunoanken@gmail.com>
2019-04-23 08:42:31 -0300

    add some probability.py's functions to main.py and reorganize the reading section so it becomes more readable

--- script/injection/main.py
+++ script/injection/main.py
@@ -19,11 +19,43 @@ def sliceList(data, start, end):
     return data[start:end]
 
 
-def insertValueInto(data, position, value):
-    prePosition = sliceList(data, 0, position)
-    posPosition = sliceList(data, position, len(data))
-    prePosition.append(value)
-    return prePosition + posPosition
+def createDictionary(keyColumn, valueColumn):
+    if(len(keyColumn) == len(valueColumn)):
+        dictionary = dict(zip(keyColumn, valueColumn))
+        return dictionary
+    else:
+        print('ERRO: listas com tamanhos diferentes')
+
+
+def minuteMapper(dictionary, interval):
+    firstKey = next(iter(dictionary))
+
+    globalMinute = int(dictionary[firstKey].split(':')[1])
+
+    # o primeiro valor do primeiro intervalo sempre será o primeiro item, de index 0
+    intervalIndexes = [0]
+    intervalCounter = 0
+
+    for index in dictionary:
+        minute = int(dictionary[index].split(':')[1])
+
+        if(minute != globalMinute):
+            globalMinute += 1
+            intervalCounter += 1
+
+        if(globalMinute == 60):
+            globalMinute = 0
+
+        if(intervalCounter == interval):
+            intervalCounter = 0
+            intervalIndexes.append(index)
+
+    return intervalIndexes
+
+
+# TODO: pegar a função que faz o mapeamento de index para minutos
+# fazer outra função que vai modificar o resultado da primeira de acordo
+# com novos dados sendo injetados pois isto modifica o valor dos índices que correspondem aos minutos
 
 
 # ataques que serão implementados: DoS e DDoS
@@ -36,6 +68,12 @@ def insertValueInto(data, position, value):
 # ========================================
 # data generation functions              =
 # ========================================
+
+def insertValueInto(data, position, value):
+    prePosition = sliceList(data, 0, position)
+    posPosition = sliceList(data, position, len(data))
+    prePosition.append(value)
+    return prePosition + posPosition
 
 
 def generateIp():
@@ -65,31 +103,36 @@ def generatePacketsOrBytes(_min, _max):
 path = '.test/1.csv'
 file = readFile(path)
 
+# read all the data
+index = readColumn(file, 'index')
 horario = readColumn(file, 'horario')
-horarioResult = insertValueInto(horario, 2, 'HorarioResult')
 ipOrigem = readColumn(file, 'ip_origem')
-ipOrigemResult = insertValueInto(ipOrigem, 2, generateIp())
 portaOrigem = readColumn(file, 'porta_origem')
-# print(min(portaOrigem))
-portaOrigemResult = insertValueInto(portaOrigem, 2, generatePort())
 ipDestino = readColumn(file, 'ip_destino')
-ipDestinoResult = insertValueInto(ipDestino, 2, 'IpDestinoResult')
 portaDestino = readColumn(file, 'porta_destino')
-# print(min(portaDestino))
-portaDestinoResult = insertValueInto(portaDestino, 2, generatePort())
 pacotes = readColumn(file, 'pacotes')
-# print(max(pacotes))
+_bytes = readColumn(file, 'bytes')
+
+timeMap = createDictionary(index, horario)
+
+# get the maximuns and minimuns necessary to keep some data quality
 packetsMax = max(pacotes)
 packetsMin = min(pacotes)
-pacotesResult = insertValueInto(
-    pacotes, 2, generatePacketsOrBytes(packetsMin, packetsMax))
-_bytes = readColumn(file, 'bytes')
-# print(min(_bytes))
 bytesMax = max(_bytes)
 bytesMin = min(_bytes)
+
+# generate and insert values
+horarioResult = insertValueInto(horario, 2, 'HorarioResult')
+ipOrigemResult = insertValueInto(ipOrigem, 2, generateIp())
+portaOrigemResult = insertValueInto(portaOrigem, 2, generatePort())
+ipDestinoResult = insertValueInto(ipDestino, 2, 'IpDestinoResult')
+portaDestinoResult = insertValueInto(portaDestino, 2, generatePort())
+pacotesResult = insertValueInto(
+    pacotes, 2, generatePacketsOrBytes(packetsMin, packetsMax))
 _bytesResult = insertValueInto(
     _bytes, 2, generatePacketsOrBytes(bytesMin, bytesMax))
 
+# write it all
 output = open('.test/test.csv', 'w')
 output.write(
     'index,horario,ip_origem,porta_origem,ip_destino,porta_destino,pacotes_ps,bytes_ps\n')
