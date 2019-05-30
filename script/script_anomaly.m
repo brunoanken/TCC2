29a5cadbab51e25a857f034d7feb112af6e34dcb
brunoanken <brunoanken@gmail.com>
2019-05-30 00:05:54 -0300

    remove the clf(reset) commands from script_anomaly and add a close() call so my machine will not run out of RAM when the script is running because all the figures were kept openned

--- script/script_anomaly.m
+++ script/script_anomaly.m
@@ -9,62 +9,68 @@ for M = 1:5
             legends = {"Entropia na primeira semana", "Baseline", "Entropia do dia com anomalias"};
 %             adicionando o baseline dos dias com anomalias aos gráficos
 %             já existentes
-            f = openfig(strcat("../imagens/dados_originais/minuto", num2str(M), "/intervalo_semana", num2str(S), "/dia", num2str(D), "/iporigem.fig"));
-            clf("reset");
+            f1 = openfig(strcat("../imagens/dados_originais/minuto", num2str(M), "/intervalo_semana", num2str(S), "/dia", num2str(D), "/iporigem.fig"));
+%             clf("reset");
             anomalies = csvread(strcat("../dados_anomalos/entropy/", num2str(M), "/", num2str(D), ".csv"), 1, 1);
             plot(1:size(anomalies, 1), anomalies(:,1), '--b');
             legend(legends, "Location", "southwest");
-            saveas(f, strcat("../imagens/dados_anomalos/minuto", num2str(M),"/intervalo_semana", num2str(S), "/dia", num2str(D), "/iporigem.jpg"));
+            saveas(f1, strcat("../imagens/dados_anomalos/minuto", num2str(M),"/intervalo_semana", num2str(S), "/dia", num2str(D), "/iporigem.jpg"));
+            close()
             %             Porta de Origem
             legends = {"Entropia na primeira semana", "Baseline", "Entropia do dia com anomalias"};
 %             adicionando o baseline dos dias com anomalias aos gráficos
 %             já existentes
-            f = openfig(strcat("../imagens/dados_originais/minuto", num2str(M), "/intervalo_semana", num2str(S), "/dia", num2str(D), "/portaorigem.fig"));
-            clf("reset");
+            f2 = openfig(strcat("../imagens/dados_originais/minuto", num2str(M), "/intervalo_semana", num2str(S), "/dia", num2str(D), "/portaorigem.fig"));
+%             clf("reset");
             anomalies = csvread(strcat("../dados_anomalos/entropy/", num2str(M), "/", num2str(D), ".csv"), 1, 1);
             plot(1:size(anomalies, 1), anomalies(:,2), '--b');
             legend(legends, "Location", "southwest");
-            saveas(f, strcat("../imagens/dados_anomalos/minuto", num2str(M),"/intervalo_semana", num2str(S), "/dia", num2str(D), "/portaorigem.jpg"));
+            saveas(f2, strcat("../imagens/dados_anomalos/minuto", num2str(M),"/intervalo_semana", num2str(S), "/dia", num2str(D), "/portaorigem.jpg"));
+            close()
             %             IP de Destino
             legends = {"Entropia na primeira semana", "Baseline", "Entropia do dia com anomalias"};
 %             adicionando o baseline dos dias com anomalias aos gráficos
 %             já existentes
-            f = openfig(strcat("../imagens/dados_originais/minuto", num2str(M), "/intervalo_semana", num2str(S), "/dia", num2str(D), "/ipdestino.fig"));
-            clf("reset");
+            f3 = openfig(strcat("../imagens/dados_originais/minuto", num2str(M), "/intervalo_semana", num2str(S), "/dia", num2str(D), "/ipdestino.fig"));
+%             clf("reset");
             anomalies = csvread(strcat("../dados_anomalos/entropy/", num2str(M), "/", num2str(D), ".csv"), 1, 1);
             plot(1:size(anomalies, 1), anomalies(:,3), '--b');
             legend(legends, "Location", "southwest");
-            saveas(f, strcat("../imagens/dados_anomalos/minuto", num2str(M),"/intervalo_semana", num2str(S), "/dia", num2str(D), "/ipdestino.jpg"));
+            saveas(f3, strcat("../imagens/dados_anomalos/minuto", num2str(M),"/intervalo_semana", num2str(S), "/dia", num2str(D), "/ipdestino.jpg"));
+            close()
             %             Porta de Destino
             legends = {"Entropia na primeira semana", "Baseline", "Entropia do dia com anomalias"};
 %             adicionando o baseline dos dias com anomalias aos gráficos
 %             já existentes
-            f = openfig(strcat("../imagens/dados_originais/minuto", num2str(M), "/intervalo_semana", num2str(S), "/dia", num2str(D), "/portadestino.fig"));
-            clf("reset");
+            f4 = openfig(strcat("../imagens/dados_originais/minuto", num2str(M), "/intervalo_semana", num2str(S), "/dia", num2str(D), "/portadestino.fig"));
+%             clf("reset");
             anomalies = csvread(strcat("../dados_anomalos/entropy/", num2str(M), "/", num2str(D), ".csv"), 1, 1);
             plot(1:size(anomalies, 1), anomalies(:,4), '--b');
             legend(legends, "Location", "southwest");
-            saveas(f, strcat("../imagens/dados_anomalos/minuto", num2str(M),"/intervalo_semana", num2str(S), "/dia", num2str(D), "/portadestino.jpg"));
+            saveas(f4, strcat("../imagens/dados_anomalos/minuto", num2str(M),"/intervalo_semana", num2str(S), "/dia", num2str(D), "/portadestino.jpg"));
+            close()
             %             Pacotes por Segundo
             legends = {"Entropia na primeira semana", "Baseline", "Entropia do dia com anomalias"};
 %             adicionando o baseline dos dias com anomalias aos gráficos
 %             já existentes
-            f = openfig(strcat("../imagens/dados_originais/minuto", num2str(M), "/intervalo_semana", num2str(S), "/dia", num2str(D), "/pacotesps.fig"));
-            clf("reset");
+            f5 = openfig(strcat("../imagens/dados_originais/minuto", num2str(M), "/intervalo_semana", num2str(S), "/dia", num2str(D), "/pacotesps.fig"));
+%             clf("reset");
             anomalies = csvread(strcat("../dados_anomalos/entropy/", num2str(M), "/", num2str(D), ".csv"), 1, 1);
             plot(1:size(anomalies, 1), anomalies(:,5), '--b');
             legend(legends, "Location", "southwest");
-            saveas(f, strcat("../imagens/dados_anomalos/minuto", num2str(M),"/intervalo_semana", num2str(S), "/dia", num2str(D), "/pacotesps.jpg"));
+            saveas(f5, strcat("../imagens/dados_anomalos/minuto", num2str(M),"/intervalo_semana", num2str(S), "/dia", num2str(D), "/pacotesps.jpg"));
+            close()
             %             Bytes por Segundo
             legends = {"Entropia na primeira semana", "Baseline", "Entropia do dia com anomalias"};
 %             adicionando o baseline dos dias com anomalias aos gráficos
 %             já existentes
-            f = openfig(strcat("../imagens/dados_originais/minuto", num2str(M), "/intervalo_semana", num2str(S), "/dia", num2str(D), "/bytesps.fig"));
-            clf("reset");
+            f6 = openfig(strcat("../imagens/dados_originais/minuto", num2str(M), "/intervalo_semana", num2str(S), "/dia", num2str(D), "/bytesps.fig"));
+%             clf("reset");
             anomalies = csvread(strcat("../dados_anomalos/entropy/", num2str(M), "/", num2str(D), ".csv"), 1, 1);
             plot(1:size(anomalies, 1), anomalies(:,6), '--b');
             legend(legends, "Location", "southwest");
-            saveas(f, strcat("../imagens/dados_anomalos/minuto", num2str(M),"/intervalo_semana", num2str(S), "/dia", num2str(D), "/bytesps.jpg"));
+            saveas(f6, strcat("../imagens/dados_anomalos/minuto", num2str(M),"/intervalo_semana", num2str(S), "/dia", num2str(D), "/bytesps.jpg"));
+            close()
         end
     end
 end
