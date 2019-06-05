7f450ec211e8dd85093a18c9ced9beec7768275d
brunoanken <brunoanken@gmail.com>
2019-06-04 21:14:10 -0300

    remove all comments from probability

--- script/probability.py
+++ script/probability.py
@@ -4,23 +4,17 @@ import sys
 import pandas as pd
 import math
 
-# lê a coluna passada como argumento e retorna seus valores em uma lista
-
 
 def readColumn(filePath, columnName):
     f = open(filePath, 'r')
     data = pd.read_csv(f)
     return data[columnName].tolist()
 
-# lê as duas colunas passadas como argumento e retorna seus valores
-
 
 def readColumns(filePath, firstColumnName, secondColumnName):
     f = open(filePath, 'r')
     data = pd.read_csv(f)
     return data[[firstColumnName, secondColumnName]]
-
-# cria um dicionário usando uma lista como chave e outra lista como valor
 
 
 def createDictionary(keyColumn, valueColumn):
@@ -29,8 +23,6 @@ def createDictionary(keyColumn, valueColumn):
         return dictionary
     else:
         print('ERRO: listas com tamanhos diferentes')
-
-# cria um histograma dos valores da lista passada como parâmetro
 
 
 def createHistogram(data):
@@ -42,20 +34,12 @@ def createHistogram(data):
             histogram[value] = 1
     return histogram
 
-# recebe um dicionário e o intervalo de tempo em minutos como parâmetros
-# vai mapear o dicionário, que deve conter o índice como chave e o horário como valor
-# cria uma lista com o índice do início de cada intervalo
-# o intervalo será medido em minutos
-# desta maneira, será possível saber de qual índice a qual índice
-# se aplica a divisão no intervalo em minutos definido
-
 
 def minuteMapper(dictionary, interval):
     firstKey = next(iter(dictionary))
 
     globalMinute = int(dictionary[firstKey].split(':')[1])
 
-    # o primeiro valor do primeiro intervalo sempre será o primeiro item, de index 0
     intervalIndexes = [0]
     intervalCounter = 0
 
@@ -75,14 +59,9 @@ def minuteMapper(dictionary, interval):
 
     return intervalIndexes
 
-# dá slice na lista de acordo com os valores passados e retorna o resultado
-
 
 def sliceList(data, start, end):
     return data[start:end]
-
-# calcula a probabilidade de ocorrência de cada item em um histograma
-# retorna o resultado em forma de dicionário
 
 
 def probability(histogram):
@@ -95,11 +74,6 @@ def probability(histogram):
     return histogram
 
 
-# calcula A FUCKING ENTROPIA DE SHANNON
-# A PORRA DA ENTROPIA DA INFORMAÇÃO
-# FINALMENTE CARALHO PORRA
-# recebe como parâmetro o dicionário com as probabilidades das chaves
-
 def entroPy(data):
     entropy = 0
     for key in data:
@@ -108,8 +82,6 @@ def entroPy(data):
     return entropy
 
 
-# range(start,end) vai de start até end - 1
-# primeiro for para percorrer os arquivos de todos os dias
 for day in range(1, 32):
     file = f'../dados_rede/data/csv_data/{day}.csv'
 
@@ -118,26 +90,16 @@ for day in range(1, 32):
 
     dic = createDictionary(key, value)
 
-    # mais um for, pra ir de acordo com os intervalos em minutos (1, 2, 3, 4, 5)
-    # vai ser o mesmo intervalo para todas as colunas
-    # mas o cálculo dos intervalos será feito uma coluna de cada vez
-    # e ao final os dados serão escritos
     for minute in range(1, 6):
         vec = minuteMapper(dic, minute)
 
-        # a lista de resultados de cada coluna vai ter o mesmo tamanho
-        # visto que o CSV é padronizado
-
-        # lista pra posterior escrita no csv
         results_ip_origem = []
         results_porta_origem = []
         results_ip_destino = []
         results_porta_destino = []
 
-        # cria a linha do csv
         result = ''
 
-        # mais um for, pra percorrer todos as colunas em que a entropia deve ser calculada
         entropyData = ['ip_origem', 'porta_origem',
                        'ip_destino', 'porta_destino']
 
@@ -169,7 +131,6 @@ for day in range(1, 32):
         results_pacotes_ps = []
         results_bytes_ps = []
 
-        # este for é para calcular a média de bytes/s e pacotes/s
         averageData = ['pacotes', 'bytes']
 
         for column in averageData:
@@ -177,7 +138,6 @@ for day in range(1, 32):
 
             count = 0
 
-            # ashdauifgaisda olha isso mano
             for i in vec:
                 res = 0
 
